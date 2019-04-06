ff63ce293fac2074deaa931b7870281706686a08
ayon <kr_ayon@icloud.com>
2019-04-06 13:08:14 +0200

    Some code cleanup

---
+++ AppStoreClone/Helpers/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  AppStoreClone
+//
+//  Created by krAyon on 06.04.19.
+//  Copyright © 2019 DocDevs. All rights reserved.
+//
+let NEW_GAMES = "https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/50/explicit.json"
+let TOP_GROSSING = "https://rss.itunes.apple.com/api/v1/us/ios-apps/top-grossing/all/50/explicit.json"
+let SOCIAL = "https://api.letsbuildthatapp.com/appstore/social"
