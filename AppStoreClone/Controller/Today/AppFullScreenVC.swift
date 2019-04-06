9ae92937b062bf912f599320c8e9d176e7283b2e
ayon <kr_ayon@icloud.com>
2019-04-06 11:59:43 +0200

    Floating controls View

--- AppStoreClone/Controller/Today/AppFullScreenVC.swift
+++ AppStoreClone/Controller/Today/AppFullScreenVC.swift
@@ -48,6 +48,8 @@ class AppFullScreenVC: UIViewController, UITableViewDelegate, UITableViewDataSou
         
         view.clipsToBounds = true
         setupTableVIew()
+        
+        setupFlotingControls()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -86,5 +88,47 @@ class AppFullScreenVC: UIViewController, UITableViewDelegate, UITableViewDataSou
         closeButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 12), size: .init(width: 80, height: 38))
         closeButton.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
         
+    }
+    
+    fileprivate func setupFlotingControls() {
+        let floatingContainerView = UIView()
+        floatingContainerView.clipsToBounds = true
+        floatingContainerView.layer.cornerRadius = 16
+        let bottomPadding = UIApplication.shared.statusBarFrame.height
+        view.addSubview(floatingContainerView)
+        floatingContainerView.anchor(top: nil, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: bottomPadding, right: 16), size: .init(width: 0, height: 100))
+        let blurVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
+        floatingContainerView.addSubview(blurVisualEffectView)
+        blurVisualEffectView.fillSuperview()
+        
+        setupFloatingSubViews(floatingContainerView)
+    }
+    
+    fileprivate func setupFloatingSubViews(_ floatingContainerView: UIView) {
+        let imageView = UIImageView(cornerradius: 16)
+        imageView.image = todayItem?.image
+        imageView.constrainHeight(constant: 68)
+        imageView.constrainWidth(constant: 68)
+        
+        let getButton = UIButton(title: "GET")
+        getButton.setTitleColor(.white, for: .normal)
+        getButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        getButton.backgroundColor = .darkGray
+        getButton.layer.cornerRadius = 16
+        getButton.constrainHeight(constant: 32)
+        getButton.constrainWidth(constant: 80)
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            imageView,
+            VerticalStackView(arrangedSubViews: [
+                UILabel(text: "Life Hack", font: .boldSystemFont(ofSize: 18)),
+                UILabel(text: "Utilizing your time", font: .boldSystemFont(ofSize: 18))
+                ], spacing: 4),
+            getButton
+            ], customSpacing: 16)
+        
+        floatingContainerView.addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 0, left: 16, bottom: 0, right: 16))
+        stackView.alignment = .center
     }
 }
